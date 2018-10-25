2bd53fe07681a5308a0d3ff279b07bdf1c93c553
Bryan Müller <bmuller0975@gmail.com>
2018-10-24 22:05:30 -0600

    initial server setup complete

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const books = [
+  {
+    title: "An example book",
+    author: "dickweed1"
+  }
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }: { url: string }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
