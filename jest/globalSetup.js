d51fa611f318e008205b7e877167b6b6877a1cb2
Bryan Müller <bmuller0975@gmail.com>
2018-11-17 16:27:21 -0700

    linting

--- jest/globalSetup.js
+++ jest/globalSetup.js
@@ -1,10 +1,12 @@
-const nock = require('nock');
+const nock = require("nock");
 module.exports = () => {
   process.env.NODE_ENV = "test";
   process.env.TEST_PORT = 4001;
   process.env.TEST_HOST = "http://localhost:4001";
   process.env.TEST_GRAPHQL_ENDPOINT = "http://localhost:4001/graphql";
-  const mock_sg = nock('https://api.sendgrid.com').post('/v3/mail/send').reply(200, {
-    message: 'success'
-  })
+  nock("https://api.sendgrid.com")
+    .post("/v3/mail/send")
+    .reply(200, {
+      message: "success"
+    });
 };
