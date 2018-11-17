8b098498a07c58115c1cea14aef35977b45d3940
Bryan Müller <bmuller0975@gmail.com>
2018-11-17 14:37:14 -0700

    linting

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   verbose: true,
   transform: {
-    "^^.+\\.tsx?$": "ts-jest",
+    "^^.+\\.tsx?$": "ts-jest"
   },
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   testRegex: "/__tests__/.*.test.(js|ts|tsx)?$",
   moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
-  globalSetup: './jest/globalSetup.js',
-  coverageReporters: [
-    "lcov",
-    "html"
-  ]
+  globalSetup: "./jest/globalSetup.js",
+  coverageReporters: ["lcov", "html"]
 };
--- jest/globalSetup.js
+++ jest/globalSetup.js
@@ -1,9 +1,6 @@
 module.exports = () => {
-  process.env.NODE_ENV = 'test';
+  process.env.NODE_ENV = "test";
   process.env.TEST_PORT = 4001;
-  process.env.TEST_HOST = 'http://localhost:4001';
-  process.env.TEST_GRAPHQL_ENDPOINT = 'http://localhost:4001/graphql'
+  process.env.TEST_HOST = "http://localhost:4001";
+  process.env.TEST_GRAPHQL_ENDPOINT = "http://localhost:4001/graphql";
 };
-
-
-
