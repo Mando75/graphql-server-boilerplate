bf3d0a9fb81d0c800a34f2ebb853b0df81489387
Bryan Müller <starwarsgeek0975@gmail.com>
2018-12-28 21:46:05 -0500

    better import

--- scripts/genSchema.ts
+++ scripts/genSchema.ts
@@ -2,7 +2,7 @@ import { generateNamespace } from "@gql2ts/from-schema";
 import * as fs from "fs";
 import * as path from "path";
 
-import { makeSchema } from "../src/utils/makeSchema";
+import { makeSchema } from "../src/utils";
 
 const types = generateNamespace("GQL", makeSchema());
 
